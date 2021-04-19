285f2be2c61754456dc024788f9281fb0db80c0a
natasharrison <natasharrison@gmail.com>
2021-04-18 17:25:33 -0700

    hour variables and css rows

--- Develop/script.js
+++ Develop/script.js
@@ -1,26 +1,49 @@
 var currentDay = moment().format('MMMM Do YYYY');
-// var enterTextEl = document.querySelector("#enter-task");
-//     console.log(timeBlockTask);
-// var saveButtonEl = document.querySelector("#saveBtn");
+
+// current hour in military time
+var currentHour = moment().format('H');
+
+// var taskInput = document.querySelector("input[name='enter-task']").value;
+// do I need to append this anywhere?
+var saveButtonEl = document.querySelector("#saveBtn");
+// Hour variables 
+var nineEl = document.querySelector("#timeNine").innerText;
+var tenEl = document.querySelector("#timeTen").innerText;
+var elevenEl = document.querySelector("#timeEleven").innerText;
+var twelveEl = document.querySelector("#timeTwelve").innerText;
+var oneEl = document.querySelector("#timeOne").innerText;
+var twoEl = document.querySelector("#timeTwo").innerText;
+var threeEl = document.querySelector("#timeThree").innerText;
+var fourEl = document.querySelector("#timeFour").innerText;
+var fiveEl = document.querySelector("#timeFive").innerText;
 
 // display current date 
 window.onload = function (){
     document.getElementById('currentDay').innerHTML = currentDay;
 };
 
-// // plan a task 
-// var planTask = function(event) {
-//     // prevent page from refreshing 
-//     event.preventDefault();
-
-//     // get value from input element 
-//     var task = enterTaskEl.value;
-// };
-
 // // save to local storage when button is clicked 
-// var buttonClickHandler = function(event){
-// // save to local storage
-// };
+saveButtonEl.addEventListener("click", function(){
+    alert("Button clicked");
+});
+// define 'tasks' in the JSON.stringify function 
 
-// // add event listener to buttons
-// saveButtonEl.addEventListener("click", buttonClickHandler);
+
+var loadTasks = function(){
+    var savedTasks = localStorage.getItem("taskInput");
+
+    savedTasks = JSON.parse(savedTasks);
+
+}
+
+// $("#taskInput .saveBtn").click(function(){
+// });
+var then = moment().hour(time);
+
+var future = moment().isBefore(then);
+var past = moment().isAfter(then);
+var present = moment().isSame(then);
+
+if (future === true) {
+    $()
+}
